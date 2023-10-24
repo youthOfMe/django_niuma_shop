2c50fc89dad4fbcbdbf6dfb0df3d9bb6fc0443b8
youthOfMe <204069080@qq.com>
2023-10-24 15:32:46 +0800

    配置redis缓存服务器

--- django_niuma_shop/django_niuma_shop/settings.py
+++ django_niuma_shop/django_niuma_shop/settings.py
@@ -125,3 +125,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 配置django-redis
+CACHES = {
+    "default": {       # 预留
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {  # 保存session数据
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = 'default'
