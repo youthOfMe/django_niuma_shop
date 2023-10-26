8c8f105d914931911cc741bbc27a46880ab4826d
youthOfMe <204069080@qq.com>
2023-10-26 22:47:06 +0800

    配置判断手机号是否存在的接口

--- django_niuma_shop/apps/users/urls.py
+++ django_niuma_shop/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.users.views import UsernameCountView
+from apps.users.views import UsernameCountView, UserModelCountView
 
 urlpatterns = [
     # 判断用户是否存在
-    path('usernames/<username:username>/count/', UsernameCountView.as_view())
+    path('usernames/<username:username>/count/', UsernameCountView.as_view()),
+    path('mobiles/<str:mobile>/count/', UserModelCountView.as_view())
 ]
--- django_niuma_shop/apps/users/views.py
+++ django_niuma_shop/apps/users/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -13,3 +15,13 @@ class UsernameCountView(View):
         # 进行返回响应
         return JsonResponse({ 'code': 0, 'count': count, 'errmsg': 'ok' })
 
+# 进行配置一个判断手机号是否已经注册的接口
+class UserModelCountView(View):
+    def get(self, request, mobile):
+        # 1. 接收手机号对手机号进行正则判断
+        if not re.match('^1[3-9]\d{9}$', mobile):
+            return JsonResponse({ 'code': 400, 'errmsg': '手机号不符合格式' })
+        # 2. 根据手机号进行查询数据库
+        count = User.objects.filter(mobile = mobile).count()
+        # 3. 返回响应
+        return JsonResponse({ 'code': 0, 'count': count, 'errmsg': 'ok' })
