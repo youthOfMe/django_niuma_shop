105d357fefabdd04484fe5981bba55da9f9c2f4d
youthOfMe <204069080@qq.com>
2023-10-29 14:20:21 +0800

    进行实现短信验证码功能

--- django_niuma_shop/apps/verifications/urls.py
+++ django_niuma_shop/apps/verifications/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.verifications.views import ImageCodeView
+from apps.verifications.views import ImageCodeView, SmsCodeView
 
 urlpatterns = [
-    path('image_codes/<uuid>/', ImageCodeView.as_view())
+    path('image_codes/<uuid>/', ImageCodeView.as_view()),
+    path('sms_codes/<mobile>/', SmsCodeView.as_view())
 ]
--- django_niuma_shop/apps/verifications/views.py
+++ django_niuma_shop/apps/verifications/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -19,3 +19,51 @@ class ImageCodeView(View):
         return HttpResponse(image, content_type='image/jpeg')
 
 # 配置短信验证码接口类
+class SmsCodeView(View):
+
+    def get(self, request, mobile):
+        # 1. 获取请求参数
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('image_code_id')
+        # 2. 验证参数
+        if not all([image_code, uuid]):
+            return JsonResponse({ 'code': 400, 'errmsg': '参数不全' })
+        # 3. 验证图片验证码
+        # 3.1 进行连接redis
+        from django_redis import get_redis_connection
+        redis_cli = get_redis_connection('code')
+        # 3.2 获取redis数据
+        redis_image_code = redis_cli.get(uuid)
+        # 3.3 进行对比图形验证码
+        if not redis_image_code:
+            return JsonResponse({ 'code': 400, 'errmsg': '图片验证码已过期或者不存在' })
+        if image_code.lower() != redis_image_code.decode().lower():
+            return JsonResponse({ 'code': 400, 'errimg': '图片验证码错误' })
+
+        # 提取发送短信的标记 看看有没有
+        send_flag = redis_cli.get('send_flag_%s'%mobile)
+
+        if send_flag:
+            return JsonResponse({ 'code': 400, 'errmsg': '不要频繁发送短信' })
+
+        # 4. 生成短信验证码
+        from random import randint
+        sms_code = '%04d'%randint(0, 9999)
+
+        # 5. 使用管道进行提交短信验证码和发送标记
+        # 5.1. 新建一个管道
+        pipeline = redis_cli.pipeline()
+        # 5.2. 管道收集指令
+        # 5.2.1. 进行保存短信验证码
+        pipeline.setex(mobile, 300, sms_code)
+        # 5.2.2 添加一个发送标记 有效期60秒
+        pipeline.setex('send_flag_%s'%mobile, 60, 1)
+        # 5.3 管道执行指令
+        pipeline.execute()
+
+        # 6. 采用celery进行异步发送短信
+        from celery_tasks.sms.tasks import  celery_send_sms_code
+        celery_send_sms_code.delay(mobile, sms_code)
+
+        # 7. 返回响应
+        return JsonResponse({ 'code': 0, 'errmsg': 'ok' })
