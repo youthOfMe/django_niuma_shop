2dfb8a4f748529c4bfc50e19842b8a3734a72315
youthOfMe <204069080@qq.com>
2023-10-31 23:13:10 +0800

    ”配置登录功能“

--- django_niuma_shop/apps/users/urls.py
+++ django_niuma_shop/apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.users.views import UsernameCountView, UserModelCountView, RegisterView
+from apps.users.views import UsernameCountView, UserModelCountView, RegisterView, LoginView
 
 urlpatterns = [
     # 判断用户是否存在
     path('usernames/<username:username>/count/', UsernameCountView.as_view()),
     path('mobiles/<str:mobile>/count/', UserModelCountView.as_view()),
-    path('register/', RegisterView.as_view())
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view())
 ]
--- django_niuma_shop/apps/users/views.py
+++ django_niuma_shop/apps/users/views.py
@@ -1,5 +1,6 @@
 import re
 
+from django.contrib.auth.hashers import check_password
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -85,3 +86,43 @@ class RegisterView(View):
         return JsonResponse({ 'code': 0, 'errmsg': '用户注册成功' })
 
         # 7. 关闭crsf防御中间件
+
+# 进行配置一个登录类
+class LoginView(View):
+
+    def post(self, request):
+        # 1. 进行获取数据
+        data = json.loads(request.body.decode())
+        username = data.get('username')
+        password = data.get('password')
+        remembered = data.get('remembered')
+
+        # 2. 进行验证数据
+        if not all([username, password]):
+            return JsonResponse({ 'code': 400, 'errmsg': '参数不全' })
+
+        # 3. 进行验证用户名和密码是否正确
+        if not(re.match('1[3-9]\d{9}', username) or re.match('[a-zA-Z_-]{5,20}', username)):
+            return JsonResponse({ 'code': 400, 'errmsg': '用户名或手机号格式错误' })
+
+        user = User.objects.filter(username = username).first() or User.objects.filter(mobile = username).first()
+        if not (user and check_password(password, user.password)):
+            return JsonResponse({ 'code': 400, 'errmsg': '用户数据不存在或者密码错误' })
+        # authenticate 用户名和密码对也返回false 很奇怪
+
+        # 4. 设置session
+        from django.contrib.auth import login
+        login(request, user)
+
+        # 5. 判断是否记住登录
+        if remembered:
+            request.session.set_expiry(None)
+        else:
+            request.session.set_expiry(0)
+
+        # 6. 返回响应和设置session
+        response = JsonResponse({ 'code':0, 'errmsg': 'ok' })
+        # 进行设置cookie
+        response.set_cookie('username', username)
+        return response
+
