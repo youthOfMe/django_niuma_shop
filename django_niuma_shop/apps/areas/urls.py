32cefbc72fdbb03333bcc464e5f8e32becf4fdf0
youthOfMe <204069080@qq.com>
2023-11-04 19:18:54 +0800

    feat: 地区查询系统完结

--- django_niuma_shop/apps/areas/urls.py
+++ django_niuma_shop/apps/areas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AreaView
+from .views import AreaView, SubAreaView
 
 urlpatterns = [
-    path('areas/', AreaView.as_view())
+    path('areas/', AreaView.as_view()),
+    path('areas/<id>/', SubAreaView.as_view()),
 ]
--- django_niuma_shop/apps/areas/views.py
+++ django_niuma_shop/apps/areas/views.py
@@ -13,7 +13,7 @@ class AreaView(View):
         province_list = cache.get('province')
         # 如果没有缓存就去数据库进行查询数据
         if not province_list:
-            provinces = Area.objects.filter()
+            provinces = Area.objects.filter(parent_id = None)
             province_list = []
             for province in provinces:
                 province_list.append({
@@ -24,3 +24,24 @@ class AreaView(View):
             cache.set('province', province_list, 24 * 3600)
         # 返回响应
         return JsonResponse({ 'code': 0, 'errmsg': 'ok', 'province_list': province_list })
+
+class SubAreaView(View):
+
+    def get(self, request, id):
+
+        # 先进行获取缓存数据
+        data_list = cache.get('city:%s'%id)
+
+        if not data_list:
+            # 1. 获取省份id 市的id 查询信息
+            up_level = Area.objects.filter(id = id).first()
+            down_level = up_level.subs.all()
+            # 2. 将对象数据转换为字典数据
+            data_list = []
+            for item in down_level:
+                data_list.append({
+                    'id': item.id,
+                    'name': item.name
+                })
+            cache.set('city:%s'%id, data_list, 24*3600)
+        return JsonResponse({ 'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': data_list} })
